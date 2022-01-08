0df865d7aa73c6aa8da242b76bfa0e2941ed5431
nabeelraza-7 <razanabeel512@gmail.com>
2022-01-08 13:37:45 +0500

    restarting the project

--- classify_pest/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClassifyPestConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'classify_pest'
--- classify_pest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('about', views.about, name="about"),
-    path('classify', views.classify_here, name="classify"),
-]
