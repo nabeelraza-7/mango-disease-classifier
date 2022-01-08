aaa254c11b5cb450c1887cc1ebbd52747264bb58
nabeelraza-7 <razanabeel512@gmail.com>
2022-01-08 12:21:27 +0500

    Should work now

--- classify_pest/urls.py
+++ classify_pest/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('about', views.about, name="about"),
-    path('classify', views.mainpage, name="index"),
+    path('classify', views.classify_here, name="classify"),
 ]
